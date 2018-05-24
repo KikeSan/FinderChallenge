41ba89a354b717db31fbe01b96511a14c804ce35
kikesan <kikin1601@hotmail.com>
2018-05-24 00:25:58 -0500

    Test FormSearch

--- public/scripts/search-form.js
+++ public/scripts/search-form.js
@@ -1,18 +1,36 @@
 function searchForm() {
     // code here
+    var map = Array.prototype.map;
+    loadJSON(JSON_FILE, revisa);
+
     document.getElementById('fch-input').addEventListener("keyup", myEventHandler, false);
     var _keypress = 0;
 
     function myEventHandler(e) {
         _keypress++
-        console.log('size: ' + this.value);
-        if(_keypress >= 2) {
+        //console.log('size: ' + this.value);
+        if (_keypress >= 2) {
             document.getElementById('fch-button').removeAttribute("disabled");
         }
-        // var keyCode = e.keyCode;
-        // console.log(e, keyCode, e.which)
-        // if (keyCode == 88) {
-        //     console.log("You pressed W!");
-        // }
+
+
+
     };
+}
+
+function revisa(bd) {
+    var arrBD = [],
+        i;
+    for (i = 0; i <= bd.data.length; i++) {
+        arrBD.push(bd.data[0].title);
+    }
+    console.log(bd.data.length);
+    console.log(bd.data[0].title);
+    console.log(arrBD);
+    var input = document.getElementById("fch-input");
+    new Awesomplete(input, {
+        list: arrBD,
+        minChars: 3,
+        maxItems: 15,
+    });
 }
